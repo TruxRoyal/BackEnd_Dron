62abe8832a02cc9cd3a125094d3877f51f287b8b
Duban Guzman <gduban2014@hotmail.com>
2025-06-21 08:02:35 -0500

    add config gps

--- app/services/websocket_service.py
+++ app/services/websocket_service.py
@@ -1,6 +1,9 @@
 from flask_socketio import SocketIO
+from .websocket.virtual_gps_ws import VirtualGPSNamespace
 
 socketio = SocketIO(cors_allowed_origins="*")
+
+socketio.on_namespace(VirtualGPSNamespace())
 
 # Importación para registrar automáticamente los eventos
 from .websocket import base_ws, flight_ws, camera_ws, stats_ws
